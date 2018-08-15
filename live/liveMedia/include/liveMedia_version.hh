b023d53cd1ce675650fd40e2c5908974867e0889
TimLand <TimLand@users.noreply.github.com>
2018-08-15 15:07:45 +0800

    提交live555源码
    http://www.live555.com/liveMedia/public/。2018-08-04 20:35

---
+++ live/liveMedia/include/liveMedia_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "liveMedia" library
+// Copyright (c) 1996-2018 Live Networks, Inc.  All rights reserved.
+
+#ifndef _LIVEMEDIA_VERSION_HH
+#define _LIVEMEDIA_VERSION_HH
+
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2018.07.01"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1530403200
+
+#endif
